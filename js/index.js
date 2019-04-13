4887ce8aae11641f38d929701060c03d82d46ff1
Robert Kulp <pedalinon@gmail.com>
2019-04-13 09:33:19 -0700

    first one done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const welcomeFunBus = document.querySelector('.intro h2')
+welcomeFunBus.addEventListener('mouseover', function() {
+	welcomeFunBus.style.fontWeight = 'bold';
+	welcomeFunBus.style.color = 'red'
+})
+welcomeFunBus.addEventListener('mouseleave', function() {
+	welcomeFunBus.style.fontWeight = 'initial';
+	welcomeFunBus.style.color = 'initial'
+})
+
+console.log(welcomeFunBus)
