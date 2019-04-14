4f446bf9587625376ad25335cdd1e93d5e987818
Robert Kulp <pedalinon@gmail.com>
2019-04-14 09:06:57 -0700

    almost done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 // Your code goes here
 /*	Events used...
-*** 	mouseover
-*** 	mouseleave
-***		dblclick
-***		keydown
-***		scroll
-***		
+*** 1	mouseover
+*** 2	mouseleave
+***	3	dblclick
+***	4	keydown
+***	5	scroll
+***	6	keybup
+*** 7	load
+***	8	click
+*** 9	resized
+***	10	mouseup	
 */
 
 // Welcome to the fun bus!
@@ -51,8 +55,7 @@ funBusPic.addEventListener('mouseleave', function(event) {
 const body = document.querySelector('body');
 body.addEventListener('keydown', function(event) {
 	if(event.keyCode === 13 || event.keyCode === 27){
-		// alert("Don't push that!");
-		newElement.textContent = ("Don't push that!")
+		newElement.textContent = "Don't push that!"
 		newElement.classList.toggle('toggle')
 	}
 })
@@ -61,7 +64,10 @@ const head = document.querySelector('header');
 const headH1 = document.querySelector('header h1')
 const navA = document.querySelectorAll('nav a')
 
-console.log(navA)
+// navA.addEventListener('click', function(event){
+// 	event.preventDefault();
+// })
+
 window.addEventListener('scroll', function() {	
 	if(scrollY > 80){
 		head.style.height = '20px'
@@ -79,5 +85,60 @@ window.addEventListener('scroll', function() {
 			navA[i].style.fontSize = '1.6em'
 		}	
 	}
+})
+
+body.addEventListener('keyup', function(event){
+	if(event.keyCode === 32){
+		// newElement.textContent = "You pushed the space bar!"
+		// newElement.classList.toggle('toggle')
+		alert('You let the space bar up!')
+		console.log('yes')
+	}
+})
+
+window.addEventListener('load', function(event){
+	body.style.backgroundColor = '#000080';
+	head.style.backgroundColor = '#663399';
+	setTimeout(function() {
+		body.style.backgroundColor = 'lightgrey';
+		head.style.backgroundColor = 'lightblue'		
+	}, 2000);
+})
+
+const buttonBottomPage = document.querySelector('.btn')
+console.log(buttonBottomPage)
+
+buttonBottomPage.addEventListener('click', function(){
+	console.log(buttonBottomPage)
+	newElement.style.top = '1600px'
+	newElement.textContent = "You'll just get burned, stay home!";
+	newElement.classList.toggle('toggle')
+	setTimeout(function(){
+		newElement.style.top = '400px';
+		newElement.classList.toggle('toggle')
+	}, 3000);
+})
+
+let resized = 0;
+window.addEventListener('resize', function(){
+	if(resized < 1){
+		console.log('resized')	
+		body.style.backgroundColor = 'red'	
+		setTimeout(function(){
+			body.style.backgroundColor = 'lightgrey'
+		}, 2000)
+	}
+	resized++
+
+	setTimeout(function(){
+		resized = 0;
+		console.log(resized)
+	}, 6000);
+})
+
+const mapImage = document.querySelector('.img-content');
+
+mapImage.addEventListener('mouseup', function(){
+	console.log('moused up')
 })
 
